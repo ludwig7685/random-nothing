1d144c74dcfaa783da1c1349f47073ec569a3c8d
ludwig7685 <139857057+ludwig7685@users.noreply.github.com>
2023-08-08 23:12:38 +0200

    Update import.py

--- import.py
+++ import.py
@@ -4,8 +4,8 @@ import subprocess
 install_command = "pip install Flask Pillow requests transformers torch torchvision torchaudio"
 subprocess.run(install_command, shell=True)
 
-# Step 2: Clone the repository
-repo_url = "https://github.com/ludwig7685/image-caption-generation-with-ai-and-api.git"
+# Step 2: Clone the repository using git protocol
+repo_url = "git@github.com:ludwig7685/image-caption-generation-with-ai-and-api.git"
 clone_command = f"git clone {repo_url}"
 subprocess.run(clone_command, shell=True)
 
@@ -13,6 +13,6 @@ subprocess.run(clone_command, shell=True)
 repo_name = repo_url.split("/")[-1].split(".git")[0]
 subprocess.run(f"cd {repo_name}", shell=True)
 
-# Step 4: Run app.py
+# Step 4: Run app.py from the image-caption-generation-with-ai-and-api folder
 run_command = "python app.py"
 subprocess.run(run_command, shell=True)
